63e783a10f332a8ee5facd9a1e80e954d6448fcf
samchat2561 <samchat2561@gmail.com>
2026-01-26 11:03:39 +0700

    update listmenu

--- lib/view/listview_menu.dart
+++ lib/view/listview_menu.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
 
+class ListMenu {
+  final String? title;
+  final String? subtitle;
+  final IconData? icon;
+
+  ListMenu({required this.title, required this.subtitle, required this.icon});
+}
+
+List<ListMenu> menus = [
+  ListMenu(title: "MENU-1", subtitle: "SUB-1", icon: Icons.person),
+  ListMenu(title: "MENU-2", subtitle: "SUB-2", icon: Icons.person_add),
+  ListMenu(title: "MENU-3", subtitle: "SUB-3", icon: Icons.wifi),
+  ListMenu(title: "MENU-4", subtitle: "SUB-4", icon: Icons.build),
+  ListMenu(title: "MENU-5", subtitle: "SUB-5", icon: Icons.build),
+  ListMenu(title: "MENU-6", subtitle: "SUB-6", icon: Icons.build),
+  ListMenu(title: "MENU-7", subtitle: "SUB-7", icon: Icons.build),
+  ListMenu(title: "MENU-8", subtitle: "SUB-8", icon: Icons.build),
+  ListMenu(title: "MENU-9", subtitle: "SUB-9", icon: Icons.build),
+  ListMenu(title: "MENU-10", subtitle: "SUB-10", icon: Icons.build),
+];
+
 class ListViewMenu extends StatelessWidget {
   const ListViewMenu({super.key});
 
@@ -15,19 +36,28 @@ class ListViewMenu extends StatelessWidget {
         child: ListView.separated(
           itemBuilder: (context, index) {
             return ListTile(
-              title: Text("MENU-1"),
-              subtitle: Text("SUB-1"),
-              leading: CircleAvatar(child: Icon(Icons.person)),
-              trailing: Icon(Icons.arrow_right),
+              title: Text("${menus[index].title}"),
+              subtitle: Text("${menus[index].subtitle}"),
+              leading: CircleAvatar(child: Icon(menus[index].icon)),
+              // trailing: Icon(Icons.arrow_right),
+              trailing: IconButton(
+                onPressed: () {
+                  debugPrint(menus[index].title);
+                },
+                icon: Icon(Icons.delete),
+              ),
               onTap: () {
-                debugPrint("MENU-1");
+                debugPrint(menus[index].title);
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context, menus[index].title);
+                }
               },
             );
           },
           separatorBuilder: (context, index) {
             return Divider(color: Colors.purple);
           },
-          itemCount: 10,
+          itemCount: menus.length,
         ),
       ),
     );
